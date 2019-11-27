bc35f7af18ab2027090906ecff2d3b3a83401a51
Pavel Rachkovsky <pavel.rachkovsky@vrpconsulting.com>
2019-11-27 19:16:38 +0300

    fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,9 @@
 const button = document.querySelector('.menu-toggle');
 
 if (button) {
-    button.addEventListener('click', () => {
-        document.body.classList.toggle('menu-opened');
-    });
+	button.addEventListener('click', () => {
+		document.body.classList.toggle('menu-opened');
+	});
 }
 
 
@@ -11,10 +11,10 @@ let products = JSON.parse(localStorage.getItem('products')) || [];
 
 function showProducts(products) {
 
-    let view = '';
+	let view = '';
 
-    for (product of products) {
-        view += `
+	for (product of products) {
+		view += `
         <li class="product">
             <a class="product-link" href="#">
                 <div class="product-header">
@@ -30,27 +30,24 @@ function showProducts(products) {
                 </div>
             </a>
         </li>`
-    }
+	}
 
-    document.querySelector('.products-container').innerHTML = view;
+	document.querySelector('.products-container').innerHTML = view;
 
 }
 
 
 function addProduct() {
-    products.push({
-        name: 'New',
-        size: 'XXL',
-        color: 'green',
-        image: './images/product2.png',
-        price: '115'
-    });
-    showProducts(products);
+	products.push({
+		name: 'New',
+		size: 'XXL',
+		color: 'green',
+		image: './images/product2.png',
+		price: '115'
+	});
+	showProducts(products);
 }
 
-document.querySelector('#add-product').addEventListener('click', () => {
-    addProduct();
-});
 
 showProducts(products);
 
@@ -62,25 +59,23 @@ const womenCheckbox = document.getElementById('checkbox-women');
 
 const searchParams = new URLSearchParams(window.location.search);
 for (let param of searchParams) {
-    if (param[0] === menCheckbox.value) {
-        menCheckbox.checked = true;
-        products = products.filter((product) => {
-            return product.gender === menCheckbox.value;
-        });
-        showProducts(products);
-    }
+	if (param[0] === menCheckbox.value) {
+		menCheckbox.checked = true;
+		products = products.filter((product) => {
+			return product.gender === menCheckbox.value;
+		});
+		showProducts(products);
+	}
 }
 
 menCheckbox.addEventListener('change', (e) => {
-    if (e.target.checked) {
-        searchParams.set(e.target.value, 'true');
-        let string = searchParams.toString();
-        window.location.search = string; //should be modified
+	let string = searchParams.toString();
+	if (e.target.checked) {
+		searchParams.set(e.target.value, 'true');
+		window.history.replaceState(null, null, window.location.origin + '?' + string);
 
-    } else {
-        //....
-    }
+	} else {
+		searchParams.set(e.target.value, 'false');
+		window.history.replaceState(null, null, window.location.origin + '?' + string);
+	}
 });
-
-
-
